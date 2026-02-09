798cd88c891f6d36c1826db0e36e35e8ac63d8f3
brokuru-commits <brokuru@gmail.com>
2026-02-09 22:11:08 +0100

    Updated the main.py file to replace 'DEIN_RAW_LINK_HIER'.

---
+++ main.py
@@ -0,0 +1,7 @@
+# Updated Content
+# This file includes a direct link to the raw content.
+# Replace "DEIN_RAW_LINK_HIER" with the actual URL.
+
+url = "https://raw.githubusercontent.com/brokuru-commits/HEUM-tec/main/main.py"
+
+# Rest of the code goes here...
